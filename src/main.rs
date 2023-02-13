efcacd32c564f13c6d100fb8f5a8a78977174146
Cosmo Bobak <cosmobobak@gmail.com>
2023-02-13 23:16:43 +0000

    move-making

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+#![allow(dead_code)]
 
-use std::io::Read;
+use std::io::{Read, Write};
 
 const LICHESS_TOKEN: &str = include_str!("../token.txt");
 const LICHESS_HOST: &str = "https://lichess.org";
@@ -22,6 +23,59 @@ fn main() {
         .send()
         .unwrap();
     
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    // result is in the form of a json string
+    // like this: {"nowPlaying":[{"fullId":"Pee5EWwqV5tu","gameId":"Pee5EWwq","fen":"rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq - 0 1","color":"black","lastMove":"d2d4","source":"friend","variant":{"key":"standard","name":"Standard"},"speed":"classical","perf":"classical","rated":false,"hasMoved":false,"opponent":{"id":"maia5","username":"BOT maia5","rating":1555},"isMyTurn":true,"secondsLeft":3600}]}
+    // println!("{body}");
+
+    // parse the json string
+    let v: serde_json::Value = serde_json::from_str(&body).unwrap();
+    // println!("{v:?}");
+
+    // get the game id
+    let game_id = v["nowPlaying"][0]["gameId"].as_str().unwrap();
+    println!("current game ID: {game_id}");
+
+    // get the fen
+    let fen = v["nowPlaying"][0]["fen"].as_str().unwrap();
+    println!("fen: {fen}");
+
+    // print the legal moves: 
+    let current_position = cozy_chess::Board::from_fen(fen, false).unwrap();
+    let mut legal_moves = Vec::new();
+    current_position.generate_moves(|piece_moves| {
+        legal_moves.extend(piece_moves);
+        false // don't abort early
+    });
+
+    print!("legal moves: ");
+    for m in &legal_moves {
+        print!("{m}, ");
+    }
+    println!();
+
+    let mut user_input = String::new();
+    print!("enter move: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut user_input).unwrap();
+    let user_input = user_input.trim();
+
+    if !legal_moves.iter().map(ToString::to_string).any(|m| m == user_input) {
+        println!("illegal move!: {user_input}");
+        return;
+    }
+
+    // post the move in the form of a json string
+    // like this: https://lichess.org/api/board/game/{gameId}/move/{move}
+
+    let mut res = client
+        .post(format!("{LICHESS_HOST}/api/board/game/{game_id}/move/{user_input}"))
+        .bearer_auth(LICHESS_TOKEN)
+        .send()
+        .unwrap();
+
     let mut body = String::new();
     res.read_to_string(&mut body).unwrap();
 
