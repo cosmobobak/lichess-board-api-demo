f88e19367ca5d7acb45b154cb71ff4818989c2b4
Cosmo Bobak <cosmobobak@gmail.com>
2023-03-05 21:55:57 +0000

    add info reporting

--- src/engine.rs
+++ src/engine.rs
@@ -1,14 +1,21 @@
 
-use std::io::{Read, Write};
+use std::io::{Write, BufRead};
+
+use log::info;
 
 pub fn main(executable_path: String) {
-    // launch the engine process
+    info!("launching engine: {executable_path}");
+
+    // launch the engine process with stdout and stdin pipes
     let mut engine = std::process::Command::new(executable_path)
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
         .spawn()
         .expect("Failed to start engine");
 
     // launch a depth-10 search by passing the command
     // "go depth 10" to the engine
+    info!("process open, sending command: go depth 10");
     engine
         .stdin
         .as_mut()
@@ -17,20 +24,34 @@ pub fn main(executable_path: String) {
         .unwrap();
 
     // read the engine's output
-    let mut output = String::new();
-    engine
+    info!("reading engine output");
+    let mut output = Vec::new();
+    let stdout = engine
         .stdout
         .as_mut()
-        .unwrap()
-        .read_to_string(&mut output)
         .unwrap();
+    let mut reader = std::io::BufReader::new(stdout);
+    reader.read_until(b'\n', &mut output).unwrap();
+    let output = String::from_utf8(output).unwrap();
 
     // print the engine's output
+    info!("received engine output.");
     println!("{output}");
 
+    // send "quit" to the engine to terminate it
+    info!("sending command: quit");
+    engine
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(b"quit\n")
+        .unwrap();
+
     // wait for the engine to finish
+    info!("waiting for engine to finish");
     let status = engine.wait().unwrap();
 
     // print the engine's exit status
+    info!("engine exited.");
     println!("Engine exited with status: {status}");
 }
